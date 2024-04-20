3415afdd11f757404e4227408e32d7917428e550
Martin Rudat <mrudat@toraboka.com>
2024-04-20 13:19:05 +1000

    Add project files.

---
+++ AsyncQueue/AsyncQueue.cs
@@ -0,0 +1,35 @@
+﻿namespace AsyncQueue;
+
+public class AsyncQueue<T>
+{
+    private readonly Queue<T> itemQueue = new();
+    private readonly Queue<TaskCompletionSource<T>> waiterQueue = new();
+
+    public async Task EnqueueAsync(T item, CancellationToken cancellationToken = default)
+    {
+        if (waiterQueue.TryDequeue(out var taskCompletionSource))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            taskCompletionSource.SetResult(item);
+            await Task.Yield();
+        }
+        else
+            itemQueue.Enqueue(item);
+    }
+
+    public async Task<T> DequeueAsync(CancellationToken cancellationToken = default)
+    {
+        if (itemQueue.TryDequeue(out var item))
+            return item;
+
+        var taskCompletionSource = new TaskCompletionSource<T>();
+
+        waiterQueue.Enqueue(taskCompletionSource);
+
+        item = await taskCompletionSource.Task.WaitAsync(cancellationToken);
+
+        await Task.Yield();
+
+        return item;
+    }
+}
